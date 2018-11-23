4197d052c437688083d5c9543a9506f58d455247
theketchupdude <tom@ascholer.org>
2018-11-23 13:35:54 -0800

    First commit

---
+++ Assets/EnemyBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour {
+
+    GameObject player;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("VR Rig");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+		transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+
+        transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+	}
+}
---
+++ Assets/sf.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Vertical") < -0.1) {
+            transform.position += Vector3.back * 1;
+        } else if (Input.GetAxis("Vertical") > 0.1) {
+            transform.position += Vector3.forward * 1;
+        }
+	}
+}
