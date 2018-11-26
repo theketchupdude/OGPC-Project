703d0ad351f5c6e87b18f0fbb534848e3a06a595
iamdylannn <45320715+iamdylannn@users.noreply.github.com>
2018-11-25 18:53:01 -0800

    VolumeController
    Just fixed some compatibility issues

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void CloseApplication ()
+    {
+        Debug.Log("Closing Application.."); // This is simply for testing purposes
+        Application.Quit(); // This only closes the application if ran by the direct .exe file
+    }
+	
+}
