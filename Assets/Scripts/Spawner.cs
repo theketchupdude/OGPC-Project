092ca6c6a504af6ee9eb3cc04f27f97f263e9e84
MagicalBean <ben.weisz13@gmail.com>
2019-02-25 09:53:38 -0800

    Extremely Basic Spawning

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public List<GameObject> spawnableObjects = new List<GameObject>();
+
+    public Vector2 timerRange;
+    public float timerCounter;
+
+	// Use this for initialization
+	void Start () {
+        timerCounter = Random.Range(timerRange.x, timerRange.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timerCounter > 0) {
+            timerCounter -= Time.deltaTime;
+        } else
+        {
+            timerCounter = Random.Range(timerRange.x, timerRange.y);
+            Spawn();
+        }
+	}
+
+    public void Spawn()
+    {
+        GameObject spawnedObject = Instantiate(spawnableObjects[Random.Range(0, spawnableObjects.Count)]);
+        spawnedObject.transform.position = transform.position;
+
+    }
+}
