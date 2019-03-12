8934f9874805e2bdb8b257ceb7090618d0a2f35a
MagicalBean <ben.weisz13@gmail.com>
2019-03-11 17:13:40 -0700

    Detecting VR and Fixing the Spawners

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,26 +9,46 @@ public class Spawner : MonoBehaviour {
     public Vector2 timerRange;
     public float timerCounter;
 
+    public bool isColiding;
+
 	// Use this for initialization
 	void Start () {
+        // Start a timer between the ranges specified in the inspector
         timerCounter = Random.Range(timerRange.x, timerRange.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // Timer
 		if (timerCounter > 0) {
             timerCounter -= Time.deltaTime;
         } else
         {
             timerCounter = Random.Range(timerRange.x, timerRange.y);
-            Spawn();
+            // If nothing is coliding with the spawner, spawn something
+            if (!isColiding)
+                Spawn();
         }
 	}
 
     public void Spawn()
     {
+        // Instantiate gameobject randomly selected from the list in the inspector
         GameObject spawnedObject = Instantiate(spawnableObjects[Random.Range(0, spawnableObjects.Count)]);
+        // Reset the gameobjects position to center it on the spawner
         spawnedObject.transform.position = transform.position;
 
+    }
+
+    // Check if something is coliding with the spawner
+    private void OnCollisionEnter(Collision collision)
+    {
+        isColiding = true;
+    }
+
+    // Set it back to clear if nothing is coliding with the spawner
+    private void OnCollisionExit(Collision collision)
+    {
+        isColiding = false;
     }
 }
