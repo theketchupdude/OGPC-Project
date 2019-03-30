f12fd0f8b7c38940c9e20b020c4080622483a675
theketchupdude <38171793+theketchupdude@users.noreply.github.com>
2019-03-29 18:02:31 -0700

    Fixed all the issues

--- Assets/Scripts/Crafting.cs
+++ Assets/Scripts/Crafting.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,20 +5,20 @@ using UnityEngine;
 public class Crafting : MonoBehaviour
 {
 	string[,] Materials = new string[3, 3];
-	
+
 	List<string[,]> Recipes = new List<string[,]>();
     List<string> Results = new List<string>();
 
-    void Start () 
+    void Start ()
 	{
         CreateRecipe(new string[,] {
-            {"none", "none", "none"}, 
-            {"none", "Stick", "none"}, 
+            {"none", "none", "none"},
+            {"none", "Stick", "none"},
             {"none", "Stick", "none"}
         }, "Banana");
 	}
-	
-	public void UpdateRecipe () 
+
+	public void UpdateRecipe ()
 	{
         UpdateMaterials();
 
@@ -27,13 +26,13 @@ public class Crafting : MonoBehaviour
             Debug.Log("Recipe crafted");
         }
 	}
-	
+
 	void CreateRecipe(string[,] recipe, string result)
 	{
 		Recipes.Add(recipe);
         Results.Add(result);
 	}
-	
+
 	void UpdateMaterials()
 	{
 		Materials[0,0] = gameObject.transform.GetChild(1).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
@@ -70,57 +69,3 @@ public class Crafting : MonoBehaviour
         return true;
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Crafting : MonoBehaviour
-{
-	int recipenum = 1;
-	
-	string[,] Materials = new string[2, 2];
-	
-	List<string[,]> Recipes;
-	
-	void Start () 
-	{
-		Recipes = new List<string[,]>();
-	}
-	
-	void Update () 
-	{
-		
-	}
-	
-	void CreateRecipe(string[,] recipe)
-	{
-		Recipes.Add(recipe);
-	}
-	
-	void UpdateMaterials()
-	{
-		Materials[0,0] = this.gameObject.transform.GetChild(1).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[0,1] = this.gameObject.transform.GetChild(2).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[0,2] = this.gameObject.transform.GetChild(3).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[1,0] = this.gameObject.transform.GetChild(4).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[1,1] = this.gameObject.transform.GetChild(5).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[1,2] = this.gameObject.transform.GetChild(6).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[2,0] = this.gameObject.transform.GetChild(7).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[2,1] = this.gameObject.transform.GetChild(8).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-		Materials[2,2] = this.gameObject.transform.GetChild(9).gameObject.GetComponent<CraftingCollider>().GetCraftingMaterial();
-	}
-
-	bool CheckRecipe()
-	{
-		for(int i = 0; i < recipenum; i++)
-		{
-			if(Materials == Recipes[i])
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-}
->>>>>>> ee2817438609973a023a06543e53f3e4dca22188
--- Assets/Scripts/CraftingCollider.cs
+++ Assets/Scripts/CraftingCollider.cs
@@ -1,52 +1,24 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CraftingCollider : MonoBehaviour 
+public class CraftingCollider : MonoBehaviour
 {
-	
-	string craftingMaterial = "none";
 
-	void OnCollisionEnter(Collision collision)
-	{
-		craftingMaterial = collision.gameObject.name;
-        gameObject.transform.parent.gameObject.GetComponent<Crafting>().UpdateRecipe();
-	}
-	
-	void OnCollisionExit(Collision collision)
-	{
-		craftingMaterial = "none";
-	}
-	
-	public string GetCraftingMaterial()
-	{
-		return craftingMaterial;
-	}
-}
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CraftingCollider : MonoBehaviour 
-{
-	
 	string craftingMaterial = "none";
 
 	void OnCollisionEnter(Collision collision)
 	{
 		craftingMaterial = collision.gameObject.name;
 	}
-	
+
 	void OnCollisionExit()
 	{
 		craftingMaterial = "none";
 	}
-	
+
 	public string GetCraftingMaterial()
 	{
 		return craftingMaterial;
 	}
 }
->>>>>>> ee2817438609973a023a06543e53f3e4dca22188
