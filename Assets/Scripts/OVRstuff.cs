8152a3e281c5c67ef5c522938c2677b031218758
theketchupdude <38171793+theketchupdude@users.noreply.github.com>
2019-04-25 21:04:10 -0700

    Got an image and made base OVR interactions

---
+++ Assets/Scripts/OVRstuff.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OVRstuff : MonoBehaviour {
+
+    public int health = 100;
+    public int hunger = 0;
+
+    [SerializeField]
+    float HungerRate = 1;
+
+    Image screentint;
+
+    // Use this for initialization
+    void Start () {
+        screentint = GameObject.Find("Tnt").GetComponent<Image>();
+
+        InvokeRepeating("DecrementHunger", HungerRate, HungerRate);
+    }
+
+    void DecrementHunger()
+    {
+        if (hunger != 0)
+        {
+            hunger -= 1;
+            health += 1;
+        }
+        else if (health != 0)
+        {
+            health -= 1;
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        screentint.color = new Color(1, 0, 0, -(health / 100.0f) + 1.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Food")
+        {
+            hunger += 25;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour {
+
+    Light lite;
+
+	// Use this for initialization
+	void Start () {
+        lite = gameObject.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lite.intensity = Random.value;
+	}
+}
