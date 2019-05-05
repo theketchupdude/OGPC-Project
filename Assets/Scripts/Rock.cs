2a7b4b479dd114e4620ffded258db1badd967e4f
theketchupdude <38171793+theketchupdude@users.noreply.github.com>
2019-05-04 20:51:29 -0700

    Ctrl-C Ctrl-V

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour {
 
+    public GameObject Flint;
+
     // Use this for initialization
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Rock")
         {
             Destroy(collision.gameObject);
+            Instantiate(Flint, gameObject.transform);
         }
     }
 }
