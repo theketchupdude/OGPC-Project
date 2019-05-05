b507b58511b786f4658559dd4add7707326df49c
MagicalBean <ben.weisz13@gmail.com>
2019-05-05 11:55:55 -0700

    Adding fruit to fruit trees and organizing hierarchy

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hand")
+        {
+            rigidbody.isKinematic = false;
+        }
+    }
+}
