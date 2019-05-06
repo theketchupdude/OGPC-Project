4ab9329a24e67553c5127a5c0db0277d102e8388
MagicalBean <ben.weisz13@gmail.com>
2019-05-05 19:29:29 -0700

    Adding headset to MenuScene

--- Assets/Scripts/DetectVR.cs
+++ Assets/Scripts/DetectVR.cs
@@ -5,6 +5,7 @@ using UnityEngine.XR;
 
 public class DetectVR : MonoBehaviour
 {
+    public static bool isOculus;
 
     public GameObject Oculus;
 
@@ -18,12 +19,14 @@ public class DetectVR : MonoBehaviour
         if (detectedHMD.ToLower().Contains("oculus"))
         {
             // HMD Must be a oculus headset
+            isOculus = true;
             WindowsMR.SetActive(false);
             Oculus.SetActive(true);
         }
         else
         {
             // There isn't an easy way to check for WindowsMR so assume if it's not oculus its WindowsMR
+            isOculus = false;
             WindowsMR.SetActive(true);
             Oculus.SetActive(false);
         }
---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour, ISelectHandler {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("SampleScene"));
+    }
+}
