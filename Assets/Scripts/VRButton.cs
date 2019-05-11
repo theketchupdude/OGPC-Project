c4a073ea226b79187945a8d259befd3e30bf6e62
MagicalBean <ben.weisz13@gmail.com>
2019-05-10 21:46:59 -0700

    Adding credit scene!

--- Assets/Scripts/VRButton.cs
+++ Assets/Scripts/VRButton.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 public class VRButton : MonoBehaviour
 {
 
-    public bool playGame, quit, craft;
+    public bool playGame, quit, craft, credit, creditBack;
+
+    public GameObject optionsMenu, mainMenu;
 
     // Use this for initialization
     void Start()
@@ -34,6 +36,12 @@ public class VRButton : MonoBehaviour
                 else if (craft)
                 {
                     Craft();
+                } else if (credit)
+                {
+                    Credit();
+                } else if (creditBack)
+                {
+                    CreditBack();
                 }
             }
         }
@@ -55,6 +63,18 @@ public class VRButton : MonoBehaviour
         print("craft");
         bool craft = GameObject.FindGameObjectWithTag("CraftingRock").GetComponent<Crafting>().craft = true;
         //StartCoroutine(WaitAndResume(craft));
+    }
+
+    void Credit()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    void CreditBack()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
     private IEnumerator WaitAndResume(bool craft)
